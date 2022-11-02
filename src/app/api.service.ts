075a0fd2f0ee491dbed93f3e57561256d41da8a9
Franco-3 <franco_33.33@hotmail.com>
2022-11-02 18:19:55 -0300

    pequeños cambios en logica de mapas cambio de lugar de boton de camara

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,22 +6,37 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  headers: HttpHeaders
+  headers: HttpHeaders;
+
+  headerDict = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*'
+  }
+
+  requestOptions = {                                                                                                                                                                                 
+    headers: new HttpHeaders(this.headerDict), 
+  };
 
   constructor( public http: HttpClient ) 
   {  
     this.headers = new HttpHeaders();
     this.headers.append("Accept", 'application/json');
     this.headers.append('Content-Type','application/json');
+    this.headers.append('Access-Control-Allow-Headers', 'Content-Type');
     this.headers.append('Access-Control-Allow-Origin','*');
   }
 
   agregarProductos(data){
-    return this.http.post('http://localhost/pp2_crud/crearProducto.php',data);
+    return this.http.post('http://localhost/pp2_crud/crearProducto.php',data);// https://2000-186-124-61-158.sa.ngrok.io
+    //return this.http.post('https://16c1-186-124-61-158.sa.ngrok.io/pp2_crud/crearProducto.php',data);
+    
   }
 
   obtenerProductos(){
-     return this.http.get('http://localhost/pp2_crud/leerProducto.php')
+     return this.http.get('http://localhost/pp2_crud/leerProducto.php');
+     //return this.http.get('https://16c1-186-124-61-158.sa.ngrok.io/pp2_crud/leerProducto.php', this.requestOptions);
   }
 
 }
--- src/app/menu/map/map.page.ts
+++ src/app/menu/map/map.page.ts
@@ -35,6 +35,8 @@ export class MapPage implements OnInit {
     const element = document.getElementById('img_mapa');
     const mapaTitle = document.getElementById('mapa_title');
     const map = document.getElementById('map');
+    const sector = document.getElementsByTagName('area');
+    
     switch (e.detail.value) {
       case 'mapa1':
           mapaTitle.innerHTML = 'Sucursal 1'; 
@@ -42,6 +44,14 @@ export class MapPage implements OnInit {
           element.setAttribute('usemap', '#map1');
           element.style.display = 'initial';
           map.setAttribute('name', 'map1');
+          sector.item(0).setAttribute('shape', 'rect');
+          sector.item(0).setAttribute('coords', '94,27,358,165');
+          sector.item(1).setAttribute('shape', 'rect');
+          sector.item(1).setAttribute('coords', '76,176,342,274');
+          sector.item(2).setAttribute('shape', 'poly');
+          sector.item(2).setAttribute('coords', '189,289,363,286,410,261,444,257,446,430,401,433,347,401,194,398,192,345');
+          sector.item(3).setAttribute('shape', 'poly');
+          sector.item(3).setAttribute('coords', '128,407,125,505,227,503,263,538,306,540,314,428,209,419,171,393');
         break;
       
       case 'mapa2':
@@ -50,6 +60,14 @@ export class MapPage implements OnInit {
           element.setAttribute('usemap', '#map2');
           element.style.display = 'initial';
           map.setAttribute('name', 'map2');
+          sector.item(0).setAttribute('shape', 'rect');
+          sector.item(0).setAttribute('coords', '70,282,16,59');
+          sector.item(1).setAttribute('shape', 'rect');
+          sector.item(1).setAttribute('coords', '163,261,82,74');
+          sector.item(2).setAttribute('shape', 'rect');
+          sector.item(2).setAttribute('coords', '175,59,231,285');
+          sector.item(3).setAttribute('shape', 'rect');
+          sector.item(3).setAttribute('coords', '38,28,209,51');
           break;
 
       case 'mapa3':
@@ -58,6 +76,14 @@ export class MapPage implements OnInit {
           element.setAttribute('usemap', '#map3');
           element.style.display = 'initial';
           map.setAttribute('name', 'map3');
+          sector.item(0).setAttribute('shape', 'rect');
+          sector.item(0).setAttribute('coords', '112,12,535,152');
+          sector.item(1).setAttribute('shape', 'rect');
+          sector.item(1).setAttribute('coords', '509,198,535,588');
+          sector.item(2).setAttribute('shape', 'rect');
+          sector.item(2).setAttribute('coords', '125,163,455,617');
+          sector.item(3).setAttribute('shape', 'rect');
+          sector.item(3).setAttribute('coords', '11,118,106,560');
           break;
 
       default:
@@ -67,9 +93,9 @@ export class MapPage implements OnInit {
   }
 
   showProduct(p, e){
-    const productoSeleccionado = "Producto " + p;
-    console.log(productoSeleccionado);
-    this.openModal(productoSeleccionado);
+    const sectorSeleccionado = p;
+    console.log(sectorSeleccionado);
+    this.openModal(sectorSeleccionado);
   }
 
   presentPopover(e: Event, t: string) {
